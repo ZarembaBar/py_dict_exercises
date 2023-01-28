72f5f0976ecef5a7c3ca0d120c2442e0e88013e8
Bartek <zaremba.bar@gmail.com>
2023-01-28 21:30:24 +0100

    fumctions fixed due to comments

--- dict_tasks.py
+++ dict_tasks.py
@@ -4,8 +4,6 @@ from typing import List, Dict
 def convert_input_lists_into_dict(keys_list: List[str], values_list: List[int]) -> Dict[str, int]:
     converted_dictionary = {}
     for position in range(len(keys_list)):
-        if len(values_list) == position:
-            values_list.append(0)
         converted_dictionary[keys_list[position]] = values_list[position]
     return converted_dictionary
 
