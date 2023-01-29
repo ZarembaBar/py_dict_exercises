3cb4ac0c0e28b4a3dd5aafefa3087da6f8a967c6
Bartek <zaremba.bar@gmail.com>
2023-01-29 17:38:08 +0100

    Function searchif for value in nested dictionaries added

--- dict_tasks.py
+++ dict_tasks.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import Dict, List
 
 
 def convert_input_lists_into_dict(keys_list: List[str], values_list: List[int]) -> Dict[str, int]:
@@ -12,3 +12,23 @@ def merge_two_dicts_into_one(dict1: Dict[str, int], dict2: Dict[str, int]) -> Di
     merged_dict = dict1.copy()
     merged_dict.update(dict2)
     return merged_dict
+
+
+def print_value_from_nested_dict(input_dict: Dict[str, Dict[str, Dict[str, Dict[str, int]]]]) -> int:
+    result = input_dict["class"]["student"]["marks"]["history"]
+    return result
+
+
+if __name__ == "__main__":
+    sampleDict = {
+        "class": {
+            "student": {
+                "name": "Mike",
+                "marks": {
+                    "physics": 70,
+                    "history": 80
+                }
+            }
+        }
+    }
+    print(print_value_from_nested_dict(sampleDict))
