dca4b0a53c1601203b4c3c12b8c4d5a310e08fa2
Bartek <zaremba.bar@gmail.com>
2023-01-29 21:37:44 +0100

    Renaming keys with test added

--- test_dict_exercises.py
+++ test_dict_exercises.py
@@ -1,6 +1,6 @@
 from dict_tasks import convert_input_lists_into_dict, merge_two_dicts_into_one, \
     print_value_from_nested_dict, create_new_dict_from_extracted_values_of_another_dict, \
-    delete_given_list_of_keys_with_their_values, search_if_value_exists_in_dict
+    delete_given_list_of_keys_with_their_values, search_if_value_exists_in_dict, rename_key_of_a_dict
 
 
 def test_convert_input_list_into_dict_happy_path():
@@ -102,3 +102,13 @@ def test_search_if_value_exists_in_dict_happy_path():
     assert search_if_value_exists_in_dict(sample_dict, 200) == "200 present in a dict"
     assert search_if_value_exists_in_dict(sample_dict, 300) == "300 present in a dict"
     assert search_if_value_exists_in_dict(sample_dict, 50) == "50 is not in a dict"
+
+
+def test_rename_key_of_a_dict_happy_path():
+    sample_dict = {
+        "name": "Kelly",
+        "age": 25,
+        "salary": 8000,
+        "city": "New york"
+    }
+    assert rename_key_of_a_dict(sample_dict) == {'name': 'Kelly', 'age': 25, 'salary': 8000, 'location': 'New york'}
