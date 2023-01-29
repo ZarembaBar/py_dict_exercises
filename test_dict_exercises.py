c4263fd714f1a06c5ddca79fb9fc43cc8cf9ec6c
Bartek <zaremba.bar@gmail.com>
2023-01-29 17:38:08 +0100

    Function searchif for value in nested dictionaries added

--- test_dict_exercises.py
+++ test_dict_exercises.py
@@ -5,10 +5,6 @@ def test_convert_input_list_into_dict_happy_path():
     assert convert_input_lists_into_dict(["one", "two", "three"], [1, 2, 3]) == {"one": 1, "two": 2, "three": 3}
 
 
-def test_convert_input_list_into_dict_empty_values_list():
-    assert convert_input_lists_into_dict(["one", "two", "three"], []) == {"one": 0, "two": 0, "three": 0}
-
-
 def test_convert_input_list_into_dict_empty_keys_list():
     assert convert_input_lists_into_dict([], [1, 2, 3]) == {}
 
@@ -32,4 +28,3 @@ def test_merge_two_dicts_into_one_with_the_same_dicts():
 def test_merge_two_dicts_into_one_when_one_of_dict_is_empty():
     assert merge_two_dicts_into_one({"one": 1, "two": 2}, {}) == {"one": 1, "two": 2}
     assert merge_two_dicts_into_one({}, {"one": 1, "two": 2}) == {"one": 1, "two": 2}
-
